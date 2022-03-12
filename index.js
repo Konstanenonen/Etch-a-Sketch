9784945da4954ded95da2e146ce4f1b1ff7d494e
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-12 14:59:18 +0200

    Got rid of prompt

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-createGrid(16);
-addListenerToClearButton();
+startApp();
+
+function startApp() {
+  createGrid(16);
+  addListenerToClearButton(16);
+  addListenerToSquareButton();
+}
+
+function addListenerToSquareButton() {
+  const squareButton = document.querySelector(".square-button");
+  squareButton.addEventListener("click", () => {
+    const squaresPerSide = Number(
+      document.querySelector("#squares-input").value
+    );
+
+    if (squaresPerSide === 0) return;
+
+    createGrid(squaresPerSide);
+    addListenerToClearButton(squaresPerSide);
+    document.querySelector("#squares-input").value = "";
+  });
+}
+
+function addListenerToClearButton(squaresPerSide) {
+  const button = document.querySelector(".clear");
+  button.addEventListener("click", () => {
+    createGrid(squaresPerSide);
+  });
+}
 
 function createGrid(squaresPerSide) {
   if (squaresPerSide > 100) squaresPerSide = 100;
@@ -12,7 +39,7 @@ function addSquares(squaresPerSide) {
   for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
-    const squareSideLength= `${100 / squaresPerSide}%`;
+    const squareSideLength = `${100 / squaresPerSide}%`;
     square.style.width = squareSideLength;
     square.style.height = squareSideLength;
     document.querySelector(".container").appendChild(square);
@@ -20,16 +47,10 @@ function addSquares(squaresPerSide) {
 }
 
 function addListenersToSquares() {
-  const squares =  document.querySelectorAll(".square");
-    squares.forEach(square => square.addEventListener("mouseover", () => {
-    square.setAttribute("class", "square filled");
-  }));
-}
-
-function addListenerToClearButton() {
-  const button = document.querySelector(".clear");
-    button.addEventListener("click", () => {
-    let squaresPerSide = Number(prompt("How many squares for each side this time?"));
-    createGrid(squaresPerSide);
-  });
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) =>
+    square.addEventListener("mouseover", () => {
+      square.setAttribute("class", "square filled");
+    })
+  );
 }
